64ace96a4948589497a755266e8df29a26212c61
andressaquintanilha.email <quintanilhalg3@gmail.com>
2022-02-15 10:15:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class frequenciasmusicais extends Component {
+  state = {
+    notas: [
+      {
+        nota: "Re",
+        frequencia: "148Hz"
+      },
+      {
+        nota: "Mi",
+        frequencia: "165Hz"
+      },
+      {
+        nota: "Fa",
+        frequencia: "349Hz"
+      },
+      {
+        nota: "Sol",
+        frequencia: "198Hz"
+      },
+      {
+        nota: "La",
+        frequencia: "349Hz"
+      },
+      {
+        nota: "Si",
+        frequencia: "249Hz"
+      },
+      {
+        nota: "Do",
+        frequencia: "132Hz"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Notas musicais e suas frequencias</h1>
+        {this.state.notas.map((item) => (
+          <div>
+            <p>Nota: {item.nota}</p>
+            <p>Frequencias: {item.frequencia}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default frequenciasmusicais;
